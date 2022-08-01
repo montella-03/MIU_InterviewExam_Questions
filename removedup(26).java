4f2cb63ac24231f90bca2d0b0924a5f17cf43cbe
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-08-01 13:08:42 +0300

    Create removedup(26).java

---
+++ removedup(26).java
@@ -0,0 +1,11 @@
+public static int removeduplicate(int[] array){
+  set<integer> set = new HashSet<>();
+  for(i=0;i<array.length;i++){
+        set.add(array[i]
+        }
+        set.forEach(e-> sout(e));
+                
+                
+//
+                
+        
