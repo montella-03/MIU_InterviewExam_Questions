c309293eec103709e435b89910b8082686a5d2bd
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-08-01 13:59:42 +0300

    Create reverseno.java

---
+++ reverseno.java
@@ -0,0 +1,24 @@
+
+
+public class array {
+    public static void main(String[] args) {
+
+        System.out.println(reverse(-1891));
+
+    }
+
+    public static int reverse(int n) {
+        int sign = -1;
+        n *= sign;
+       int reverse = 0;
+       int rem;
+       while (n != 0) {
+           rem = n % 10;
+           reverse = (reverse * 10) + rem;
+           n = n / 10;
+
+       }
+       return reverse * sign;
+    }
+    
+}
